bfad567079e8243073e1e13efa74fc84a6c515f2
DickTator97 <107931017+DickTator97@users.noreply.github.com>
2022-08-22 04:39:27 +0300

    Started Working On Main Menu
    Work in progress

---
+++ Platformer Final Project Redo/Assets/Scripts/On_Play_Click.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class On_Play_Click : MonoBehaviour
+{
+    public void OnMouseDown()
+    {
+        Debug.Log("IS Clicked");
+    }
+}
